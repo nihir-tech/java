27fb9b01acee60b6ebb6e0cb1a1bc286f9741ae2
Nihir Sartanpara <sartanparanihir@gmail.com>
2025-12-28 11:08:10 +0530

    Create p22.java

---
+++ p22.java
@@ -0,0 +1,12 @@
+public class MethodWithParameter {
+
+    // method with parameters
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("Nihir");   // passing argument
+        greet("Alice");
+    }
+}

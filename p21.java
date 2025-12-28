e63731d2d83b0617b455f33d1348887dff77ca3e
Nihir Sartanpara <sartanparanihir@gmail.com>
2025-12-28 11:06:38 +0530

    Create p21.java

---
+++ p21.java
@@ -0,0 +1,11 @@
+public class MethodExample {
+
+    // method
+    static void greet() {
+        System.out.println("Hello, Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet(); // method call
+    }
+}

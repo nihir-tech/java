d491f30f9e7538bd12ec5af7f76c400b3089ef80
Nihir Sartanpara <sartanparanihir@gmail.com>
2025-12-28 11:00:19 +0530

    Create p17.java

---
+++ p17.java
@@ -0,0 +1,9 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}

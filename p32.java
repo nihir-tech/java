73805db227a9fe5692c0673378de1c3f780f8366
Nihir Sartanpara <sartanparanihir@gmail.com>
2026-03-05 12:25:19 +0530

    Create p32.java

---
+++ p32.java
@@ -0,0 +1,16 @@
+class Main
+{ 
+    static void divide() throws ArithmeticException 
+    { 
+        int a = 10 / 0; 
+    } 
+
+    public static void main(String[] args) 
+    { 
+        try { 
+            divide(); 
+        } catch (ArithmeticException e) { 
+            System.out.println("Exception handled in main");         
+        } 
+    } 
+}

84d380b76823dd70ef5c015526210e7abce46e91
Nihir Sartanpara <sartanparanihir@gmail.com>
2026-03-05 12:24:17 +0530

    Create P31.java

---
+++ P31.java
@@ -0,0 +1,20 @@
+class InvalidAgeException extends Exception{
+    InvalidAgeException(String msg){
+        super(msg);
+    }
+}class Main{
+    static void checkAge(int age) throws InvalidAgeException{
+        if (age < 18){
+            throw new InvalidAgeException("Age must be 18 or above");
+        }else{
+            System.out.println("Valid age");
+        }
+    }
+    public static void main(String[] args){
+        try{
+            checkAge(16);
+        }catch (InvalidAgeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
